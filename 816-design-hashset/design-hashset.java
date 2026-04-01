5fd48f600c22615c8a0b0dc94b3e683a1c8f61b2
vtu25706-cmyk <vtu25706@veltech.edu.in>
2026-04-01 09:48:21 +0530

    Time: 13 ms (74.29%) | Memory: 54 MB (28.25%) - LeetSync

---
+++ 816-design-hashset/design-hashset.java
@@ -0,0 +1,28 @@
+class MyHashSet {
+
+    private boolean[] storage;
+
+    public MyHashSet() {
+        storage = new boolean[1_000_001]; // Initialize array for keys in range [0, 1000000]
+    }
+
+    public void add(int key) {
+        storage[key] = true; // Mark key as present
+    }
+
+    public void remove(int key) {
+        storage[key] = false; // Mark key as absent
+    }
+
+    public boolean contains(int key) {
+        return storage[key]; // Check if key is present
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
