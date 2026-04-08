dd001ab854947d21633891d579a338c57417d314
vtu25706-cmyk <vtu25706@veltech.edu.in>
2026-04-08 10:03:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (62.75%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] result = new int[nums.length];
-        
-        result[0] = nums[0];  // first element
-        
-        for (int i = 1; i < nums.length; i++) {
-            result[i] = result[i-1] + nums[i];  
+
+        for(int i = 1; i < nums.length; i++){
+            nums[i] += nums[i - 1];
         }
-        
-        return result;
+
+        return nums;
     }
 }
+    
+
