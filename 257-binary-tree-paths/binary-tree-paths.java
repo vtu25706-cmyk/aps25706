c13ba0cd01122d96eb67f50dc7b1048fb08d770a
vtu25706-cmyk <vtu25706@veltech.edu.in>
2026-04-17 09:41:19 +0530

    Time: 3 ms (80.04%) | Memory: 44.1 MB (74.31%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<String> li=new ArrayList<>();
+    StringBuilder sb=new StringBuilder();
+    public List<String> binaryTreePaths(TreeNode root) {
+        dfs(root);
+        return li;
+    }
+    public void dfs(TreeNode root){
+        int len = sb.length();
+        if(root.left==null && root.right==null){
+            sb.append(root.val);
+            li.add(sb.toString());
+            sb.setLength(len);
+            return;
+        }
+        sb.append(root.val+"->");
+        if(root.left!=null){
+            dfs(root.left);
+        }
+        if(root.right!=null){
+            dfs(root.right);
+        }
+        sb.setLength(len);
+        }
+}
