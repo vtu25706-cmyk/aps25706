d341d225c48a29ca9e11aabcc4160c177953319e
vtu25706-cmyk <vtu25706@veltech.edu.in>
2026-04-17 09:20:28 +0530

    Time: 30 ms (73.95%) | Memory: 54 MB (87.64%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,22 @@
+class StockSpanner {
+    Stack<int[]> st;
+
+    public StockSpanner() {
+       this.st=new Stack<>();
+    }
+    public int next(int price) {
+        int result=1;
+        while(!st.isEmpty()&&st.peek()[0]<=price){
+            result+=st.pop()[1];
+            
+        }
+        st.add(new int[]{price,result});
+        return result;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
